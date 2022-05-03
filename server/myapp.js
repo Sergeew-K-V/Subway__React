937dd830850b751fb533c564f5a86b6972767f81
Kirill <sergeew_k@mail.ru>
2022-05-03 18:59:26 +0300

    connection to db plus added dep

--- server/myapp.js
+++ server/myapp.js
@@ -1,16 +1,31 @@
 const data = require('../data.json')
 
 const express = require('express')
+
+const mongoose = require('mongoose')
+const mongoDb = 'http://localhost:27017/'
 const app = express()
-const port = 2323
+const PORT = 2323
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/data', (req, res) => {
-  res.send(data)
-})
+async function startServer() {
+  try {
+    await mongoose.connect(mongoDb, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    })
+    app.get('/', (req, res) => {
+      res.send('Hello World!')
+    })
+    app.get('/data', (req, res) => {
+      res.send(data)
+    })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    app.listen(PORT, () => {
+      console.log(`Example app listening on port ${PORT}`)
+    })
+  } catch (error) {
+    console.log('error', error)
+    throw new Error(error)
+  }
+}
+startServer()
