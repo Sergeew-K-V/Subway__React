a673f895353b24118af93bd4af8ef53419b130c7
Kirill <sergeew_k@mail.ru>
2022-05-04 14:47:20 +0300

    main component

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div class='main__content' id='root-subMain-right'>
+      <div class='container-content'>
+        <div class='main__flex'>{/* Product */}</div>
+      </div>
+    </div>
+  )
+}
+export default Main
