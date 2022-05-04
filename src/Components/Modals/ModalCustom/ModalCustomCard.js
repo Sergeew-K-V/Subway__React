67639eef1efcafa9f3eff9f80c05eb30d71adb3e
Kirill <sergeew_k@mail.ru>
2022-05-04 15:08:05 +0300

    content card

--- src/Components/Modals/ModalCustom/ModalCustomCard.js
+++ src/Components/Modals/ModalCustom/ModalCustomCard.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
 function ModalCustomCard() {
-  return
+  return (
+    <div
+      class="modal__content-card 
+  {
+            this.selected === true ? 'select' : ''
+          }"
+      //   id='${this.id}'
+    >
+      <div class='content-card__block'>
+        <div class='content-card__img'>
+          {/* <img src='/src/img${this.image}' alt='el-15cm' /> */}
+        </div>
+        {/* <div class='content-card__text'>${this.name}</div> */}
+        {/* <div class='content-card__price'>Цена: ${this.price} руб.</div> */}
+      </div>
+    </div>
+  )
 }
 
 export default ModalCustomCard
