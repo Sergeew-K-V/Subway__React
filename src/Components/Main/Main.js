efb7bad7a51ce4a275e71297e0e085ddb37ccaf1
Kirill <sergeew_k@mail.ru>
2022-05-17 11:33:22 +0300

    loader changeing in Main

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -11,34 +11,47 @@ function Main() {
     console.log('render', counter)
     counter++
   })
-  // async function getterHandler() {
-  //   // URLSearchParams
-  //   try {
-  //     const data = await request('/products', 'GET')
+  async function getterHandler() {
+    // URLSearchParams
+    try {
+      const data = await request('/products', 'GET')
 
-  //     if (data !== undefined && data !== null && !loading) {
-  //       setMainData(data.products)
-  //     }
-  //   } catch (e) {}
-  // }
+      if (data !== undefined && data !== null && !loading) {
+        setMainData(data.products)
+      }
+    } catch (e) {}
+  }
 
-  // useEffect(() => {
-  //   getterHandler()
-  // }, [])
+  useEffect(() => {
+    setTimeout(() => {
+      getterHandler()
+    }, 2000)
+  }, [])
   return (
     <div className='main__content' id='root-subMain-right'>
       <div className='container-content'>
         <div className='main__flex'>
-          {loading && <Loader></Loader>}
+          {/* {loading && <Loader></Loader>} */}
           {mainData.length === 0 ? (
-            <div>
-              <span>
-                {' '}
-                <strong>There are no products!</strong>{' '}
-              </span>
-            </div>
+            // <div>
+            //   <span>
+            //     <strong>
+            //       There are no products!<Loader></Loader>
+            //     </strong>
+            //   </span>
+            // </div>
+            <Loader></Loader>
           ) : (
-            mainData
+            mainData.map((el) => (
+              <Product
+                key={el._id}
+                id={el._id}
+                name={el.name}
+                price={el.price}
+                description={el.description}
+                imageFile={el.imageFile}
+              ></Product>
+            ))
           )}
           {/* {console.log('mainData', mainData)} */}
           {/* {mainData.length !== 0 &&
