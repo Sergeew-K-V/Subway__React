2e49128d4b8a47d67891af33f453b04925b8e92f
Kirill <sergeew_k@mail.ru>
2022-05-20 16:10:30 +0300

    to ones style

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../css/loader.css'
-export default () => {
+
+export default function Loader() {
   return (
     <div className='loader__wrapper'>
       <div className='lds-dual-ring'></div>
