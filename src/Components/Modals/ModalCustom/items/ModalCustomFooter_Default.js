369eef899191a2c475aeaf8b28bbcd64b47a98f2
Kirill <sergeew_k@mail.ru>
2022-05-20 16:58:03 +0300

    footers for modalCustom

---
+++ src/Components/Modals/ModalCustom/items/ModalCustomFooter_Default.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ModalCustomFooter_Default({ customProduct }) {
+  return (
+    <div className='modal__footer' id='modal-total-bottom-root'>
+      <div className='modal__total-price'>
+        <span>Итого: {customProduct.price} руб.</span>
+      </div>
+    </div>
+  )
+}
---
+++ src/Components/Modals/ModalCustom/items/ModalCustomFooter_Total.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import plus from '../../../../img/plus-solid.svg'
+import minus from '../../../../img/minus-solid.svg'
+
+export default function ModalCustomFooter_Total({ customProduct, setQuantity }) {
+  return (
+    <div className='modal__footer' id='modal-total-bottom-root'>
+      <div className='modal__btn-block'>
+        <div className='modal-block__text'>Количество</div>
+        <div className='modal-block__btns-list'>
+          <button
+            className='btns-modal__btn'
+            onClick={(e) => customProduct.quantity !== 0 && setQuantity(customProduct.quantity - 1)}
+          >
+            <img src={minus} className='fa-solid fa-minus'></img>
+          </button>
+          <input
+            type='number'
+            className='btns-modal__btn modal-input'
+            value={customProduct.quantity}
+            onChange={(e) => {
+              if (e.target.value > 0) setQuantity(e.target.value)
+            }}
+          />
+          <button
+            className='btns-modal__btn'
+            onClick={(e) => {
+              setQuantity(customProduct.quantity + 1)
+            }}
+          >
+            <img src={plus} className='fa-solid fa-plus'></img>
+          </button>
+        </div>
+      </div>
+      <div className='modal__total-price'>
+        <span>Итого: {customProduct.price} руб.</span>
+        <div className='modal__btn-to-basket'>
+          <button className='btn-to-basket__btn'>В корзину</button>
+        </div>
+      </div>
+    </div>
+  )
+}
