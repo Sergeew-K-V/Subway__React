09731e5fde387cec150736f44b2d5f076d4621f1
Kirill <sergeew_k@mail.ru>
2022-05-20 16:58:16 +0300

    re styled

--- src/Components/Modals/ModalCustom/items/ModalLoader.js
+++ src/Components/Modals/ModalCustom/items/ModalLoader.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../../../../css/modalLoader.css'
-export default () => {
+
+export default function ModalLoader() {
   return (
     <div className='modalLoader__wrapper'>
       <div className='modalLoader__lds-dual-ring'></div>
