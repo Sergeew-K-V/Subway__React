a3368f50244e0f5f1aa2989ed9d78b2e8e61680e
Kirill <sergeew_k@mail.ru>
2022-05-23 10:41:42 +0300

    can use classNames as styling classes

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import classNames from 'classnames'
 import '../css/loader.css'
 
 export default function Loader() {
   return (
-    <div className='loader__wrapper'>
-      <div className='lds-dual-ring'></div>
+    <div className={classNames({ loader__wrapper: true })}>
+      <div className={classNames({ ldsDualRing: true })}></div>
     </div>
   )
 }
