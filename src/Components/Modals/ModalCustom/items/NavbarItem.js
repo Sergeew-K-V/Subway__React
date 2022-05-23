7cdd3dd841e646250cfb7f6e2561bf0b08a427b9
Kirill <sergeew_k@mail.ru>
2022-05-23 09:23:01 +0300

    used classNames for style

--- src/Components/Modals/ModalCustom/items/NavbarItem.js
+++ src/Components/Modals/ModalCustom/items/NavbarItem.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import ClassNames from 'classnames'
+// classNames
 export default function NavbarItem({ text, counter, currentPage, setCurrentPage }) {
+  // npm -D i? npm i ?
+  // {
+  //   navbar__item: true,
+  //   selected: currentPage === counter
+  // }
+  const styles = ClassNames({
+    navbar__item: true,
+    selected: currentPage === counter,
+  })
+
   return (
     <li
-      className={currentPage === counter ? 'navbar__item selected' : 'navbar__item'}
+      // className={currentPage === counter ? 'navbar__item selected' : 'navbar__item'}
+      className={styles}
       onClick={(e) => {
         setCurrentPage(Number(e.target.id.slice(-1)))
       }}
